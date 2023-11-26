85d23295c6e14f6078794733356a0fc26978736d
Martin Palkov <martin.palkov@gmail.com>
2023-11-26 21:41:06 +0100

    iteration 4 done

---
+++ src/components/BeerCard.jsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+import "./beercard.css";
+
+const BeerCard = (props) => {
+	const {image_url, name, tagline, contributed_by, _id} = props.beerData;
+
+	return (
+		<Link to={`/beers/:${_id}`} className="width-100">
+		<article className="beer-card width-100">
+			<div className="beer-img" >
+				<img src={image_url} alt={name} />
+			</div>
+			<div className="beer-info">
+				<h2>{name}</h2>
+				<p>{tagline}</p>
+				<p>Created by: {contributed_by}</p>
+			</div>
+			
+		</article>
+		<hr />
+		</Link>
+	);
+}
+
+export default BeerCard;
--- src/pages/AllBeersPage.jsx
+++ src/pages/AllBeersPage.jsx
@@ -1,10 +1,60 @@
+import { useState, useEffect } from "react";
+import BeerCard from "../components/BeerCard";
 import Navbar from "../components/Navbar";
+const apiURL_allBeers = "https://ih-beers-api2.herokuapp.com/beers"
+
+/* const getBeers = async(Beers, setBeers) => {
+	try {
+		const response = await fetch(apiURL_allBeers);
+		const beersJson = await response.json();
+		return beersJson;
+	}
+	catch (err) {
+		console.log(err);
+	}
+} */
 
 function AllBeersPage() {
+	const [beers, setBeers] = useState([]);
+
+	useEffect( () => {
+		const getBeers = async() => {
+			try {
+				const response = await fetch(apiURL_allBeers);
+				const beersJson = await response.json();
+				setBeers(beersJson);
+				console.log("GETBEERS", beersJson);
+			}
+			catch (err) {
+				console.log(err);
+			}
+		}
+		getBeers();
+
+	}, []);
 
 	return (
 		<>
-		<h1>All Beers</h1>
+			{ beers[0] ?
+				<>
+					{
+						beers.map((eachBeer) => {
+							return (
+								<BeerCard key={eachBeer._id} beerData={eachBeer} />
+								);
+						})
+					}
+				</> :		
+			<h1>...Loading.</h1>
+			}	
+		
+		
+		
+		{/* {beers.map((eachBeer) => {
+			return (
+				<BeerCard beerData={eachBeer} />
+			);
+		})} */}
 		</>
 	);
 }
