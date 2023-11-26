baa065eccc378a7446eace045a556832a7366637
Martin Palkov <martin.palkov@gmail.com>
2023-11-26 14:11:09 +0100

    iteration 1 fix

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,13 @@ import BeerDetailsPage from "./pages/BeerDetailsPage";
 function App() {
   return (
     <div id="App">
-		<Route path="/" element={<HomePage />} />
-		<Route path="/beers" element={<AllBeersPage />} />
-		<Route path="/random-beer" element={<RandomBeerPage />} />
-		<Route path="/new-beer" element={<AddBeerPage />} />
-		<Route path="/beers/:beerId" element={<BeerDetailsPage/>} />
+		<Routes>
+			<Route path="/" element={<HomePage />} />
+			<Route path="/beers" element={<AllBeersPage />} />
+			<Route path="/random-beer" element={<RandomBeerPage />} />
+			<Route path="/new-beer" element={<AddBeerPage />} />
+			<Route path="/beers/:beerId" element={<BeerDetailsPage/>} />
+		</Routes>
     </div>
   );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,40 @@
-function HomePage() {}
+import { Link } from "react-router-dom";
+
+function HomePage() {
+
+	return (
+		<>
+		<Link to="/beers">
+			<article className="home-button">
+				<img src="/beers.png" />
+				<h2>All Beers</h2>
+				<p>Show all beers here</p>
+			</article>
+		</Link>
+		<Link to="/beers">
+			<article className="home-button">
+				<img src="/beers.png" />
+				<h2>All Beers</h2>
+				<p>Show all beers here</p>
+			</article>
+		</Link>
+		<Link to="/beers">
+			<article className="home-button">
+				<img src="/beers.png" />
+				<h2>All Beers</h2>
+				<p>Show all beers here</p>
+			</article>
+		</Link>
+		</>
+	)
+}
 
 export default HomePage;
+
+
+/* In your App.jsx set up the routes that render the following pages:
+Route /, which renders the HomePage component
+Route /beers, which renders the AllBeersPage component.
+Route /random-beer, which renders the RandomBeerPage component.
+Route /new-beer, which renders the AddBeerPage component.
+Route /beers/:beerId, which renders the BeerDetailsPage component. */
